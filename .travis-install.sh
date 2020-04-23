420ba39d73cbfc99ffcbcbc555281d62d68389e7
Joffrey Huguet <huguet@adacore.com>
2020-04-23 17:27:10 +0200

    Add Travis CI support for the DAIDALUS_integration branch
    Use non-gcov uxas to run the testsuite

--- .travis-install.sh
+++ .travis-install.sh
@@ -4,13 +4,13 @@ set -x
 mkdir -p $TOOLS_DIR
 cd $TOOLS_DIR
 
-echo $TRAVIS_BUILD_DIR
-
 # Clone OpenUxAS-bootstrap repository, pull the last changes if it exists
 if ! [ -d OpenUxAS-bootstrap ]
 then
     git clone https://github.com/AdaCore/OpenUxAS-bootstrap.git;
     envsubst < $TRAVIS_BUILD_DIR/.travis-bootstrap.patch
+    cd OpenUxAS-bootstrap;
+    git checkout DAIDALUS_integration;
     patch -d$TOOLS_DIR/OpenUxAS-bootstrap/ -p0 < $TRAVIS_BUILD_DIR/.travis-bootstrap.patch;
 
 else
@@ -22,28 +22,3 @@ fi
 pip install wheel
 pip install zmq
 pip install git+https://github.com/AdaCore/e3-core.git
-
-# If not already present, download the GNAT Community installer and the helper
-# scripts to use it headless.
-if ! [ -f $GNAT_INSTALLER ]
-then
-    wget $GNAT_LINUX_INSTALLER_URL -O $GNAT_INSTALLER
-fi
-if ! [ -d gnat_community_install_script ]
-then
-   git clone https://github.com/AdaCore/gnat_community_install_script.git;
-else
-   (cd gnat_community_install_script && git pull)
-fi
-
-# If not already installed, install GNAT Community. The script does not work if
-# the installation directory already exists, so remove it first.
-if ! [ -f "$INSTALL_DIR/bin/gprbuild" ]
-then
-   rm -rf "$INSTALL_DIR"
-   sh gnat_community_install_script/install_package.sh \
-      "$GNAT_INSTALLER" "$INSTALL_DIR"
-fi
-
-# Log info about the toolchain we use
-which gprbuild && gprbuild --version
